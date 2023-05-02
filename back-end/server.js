50ca004490b1bc9c594494b9634240acc9d446a6
THV <116940124+thv-24@users.noreply.github.com>
2023-05-02 17:36:55 +0530

    back-end

---
+++ back-end/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = 8080
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+app.listen(PORT, () => console.log(`listening on port http://localhost:${PORT}`))
